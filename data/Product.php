85e00809564ac41f640c3334e10adcd936d3a19f
DrRhea <czajr13@gmail.com>
2024-04-26 21:55:38 +0700

    overriding

--- data/Product.php
+++ data/Product.php
@@ -4,34 +4,44 @@
     public  $title,
             $author,
             $publisher,
-            $price,
-            $pages,
-            $hours;
+            $price;
 
-    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0, $pages = 0, $hours = 0) {
+    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0) {
       $this->title = $title;
       $this->author = $author;
       $this->publisher = $publisher;
       $this->price = $price;
-      $this->pages = $pages;
-      $this->hours = $hours;
     }
     public function getLabel() {
       return "$this->author, $this->publisher";
     }
 
     public function getFullLabel() {
-      return "{$this->title} | {$this->getLabel()} (Rp{$this->price})";
+      return "$this->title | {$this->getLabel()} (Rp$this->price)";
     }
   }
 
   class Comic extends Product {
+    public $pages;
+
+    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0, $pages = 0) {
+      parent::__construct($title, $author, $publisher, $price);
+
+      $this->pages = $pages;
+    }
     public function getFullLabel() {
-      return "Comic : {$this->title} | {$this->getLabel()} (Rp{$this->price}) - {$this->pages} pages\n";
+      return "Comic : " . parent::getFullLabel() . " - {$this->pages} pages\n";
     }
   }
   class Game extends Product {
+    public $hours;
+
+    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0, $hours = 0) {
+      parent::__construct($title, $author, $publisher, $price);
+
+      $this->hours = $hours;
+    }
     public function getFullLabel() {
-      return "Game : {$this->title} | {$this->getLabel()} (Rp{$this->price}) ~ {$this->hours} hours\n";
+      return "Game : " . parent::getFullLabel() . " ~ {$this->hours} hours\n";
     }
   }
