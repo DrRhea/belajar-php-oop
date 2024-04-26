6f00f604c6d65d65c4298a8810da43ae8bdc3fba
DrRhea <czajr13@gmail.com>
2024-04-26 20:03:17 +0700

    Constructor

--- Object.php
+++ Object.php
@@ -2,16 +2,8 @@
 
   require_once ('./data/Product.php');
 
-  $Product1 = new Product();
-  $Product1->title = "Naruto";
-  $Product1->author = "Masashi Kishimoto";
-  $Product1->publisher = "Shonen Jump";
-  $Product1->price = 35000;
+  $Product1 = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 35000);
   echo "Comic : " . $Product1->getLabel();
 
-  $Product2 = new Product();
-  $Product2->title = "Uncharted";
-  $Product2->author = "Neil Druckmann";
-  $Product2->publisher = "Sony Computer";
-  $Product2->price = 250000;
+  $Product2 = new Product("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
   echo "Game : " . $Product2->getLabel();
--- data/Product.php
+++ data/Product.php
@@ -1,11 +1,17 @@
 <?php
 
   class Product{
-    public  $title = "title",
-            $author = "author",
-            $publisher = "publisher",
-            $price = 0;
+    public  $title,
+            $author,
+            $publisher,
+            $price;
 
+    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0){
+      $this->title = $title;
+      $this->author = $author;
+      $this->publisher = $publisher;
+      $this->price = $price;
+    }
     public function getLabel() {
       return "$this->author, $this->publisher\n";
     }
