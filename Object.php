074bd2584947b38a4a64a2dca4a5b701eeff6994
DrRhea <czajr13@gmail.com>
2024-04-27 00:12:24 +0700

    abstract class

--- Object.php
+++ Object.php
@@ -3,13 +3,13 @@
   require_once ('./data/Product.php');
 
   $Product1 = new Comic("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-  echo $Product1->getFullLabel();
-
   $Product2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
-  echo $Product2->getFullLabel();
 
-  echo "\n";
+  $printProduct = new printProductInfo();
+  $printProduct->addProduct($Product1);
+  $printProduct->addProduct($Product2);
+  echo $printProduct->printProduct();
 
-  $Product2->setTitle("Spiderman");
-  echo $Product2->getTitle();
+
+
 
