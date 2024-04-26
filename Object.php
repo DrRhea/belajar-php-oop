211e04d97bfff45025c53c20e5dd57ad7e991a68
DrRhea <czajr13@gmail.com>
2024-04-26 21:04:07 +0700

    inheritance problems

--- Object.php
+++ Object.php
@@ -2,15 +2,19 @@
 
   require_once ('./data/Product.php');
 
-  $Product1 = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 35000);
+  $Product1 = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 35000, 100, 0, "Comic");
 //  echo "Comic : " . $Product1->getLabel();
+  echo $Product1->getFullLabel();
 
-  $Product2 = new Product("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+  $Product2 = new Product("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 //  echo "Game : " . $Product2->getLabel();
+  echo $Product2->getFullLabel();
 
-  $ProductInfo1 = new PrintProductInfo();
-  echo $ProductInfo1->printProduct($Product1);
+//  $ProductInfo1 = new PrintProductInfo();
+//  echo $ProductInfo1->printProduct($Product1);
+//
+//  $ProductInfo2 = new PrintProductInfo();
+//  echo $ProductInfo2->printProduct($Product2);
 
-  $ProductInfo2 = new PrintProductInfo();
-  echo $ProductInfo2->printProduct($Product2);
+
 
--- data/Product.php
+++ data/Product.php
@@ -4,16 +4,33 @@
     public  $title,
             $author,
             $publisher,
-            $price;
+            $price,
+            $pages,
+            $hours,
+            $type;
 
-    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0){
+    public function __construct($title = "title", $author = "author", $publisher = "publisher", $price = 0, $pages = 0, $hours = 0, $type = "type"){
       $this->title = $title;
       $this->author = $author;
       $this->publisher = $publisher;
       $this->price = $price;
+      $this->pages = $pages;
+      $this->hours = $hours;
+      $this->type = $type;
     }
     public function getLabel() {
       return "$this->author, $this->publisher";
+    }
+
+    public function getFullLabel() {
+      $text = "{$this->type} : {$this->title} | {$this->getLabel()} (Rp{$this->price}) ";
+
+      if($this->type == "Comic")
+        $text .= "- {$this->pages} pages\n";
+      else if($this->type == "Game")
+        $text .= "~ {$this->hours} hours\n";
+
+      return $text;
     }
   }
 
