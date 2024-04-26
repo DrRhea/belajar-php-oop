1dc1f0b7ae49d42c85cb3defb51c2ec2cc64ed3a
DrRhea <czajr13@gmail.com>
2024-04-26 19:42:03 +0700

    Property and Method

--- Object.php
+++ Object.php
@@ -2,6 +2,16 @@
 
   require_once ('./data/Product.php');
 
-  $Object = new Product();
+  $Product1 = new Product();
+  $Product1->title = "Naruto";
+  $Product1->author = "Masashi Kishimoto";
+  $Product1->publisher = "Shonen Jump";
+  $Product1->price = 35000;
+  echo "Comic : " . $Product1->getLabel();
 
-  var_dump($Object);
+  $Product2 = new Product();
+  $Product2->title = "Uncharted";
+  $Product2->author = "Neil Druckmann";
+  $Product2->publisher = "Sony Computer";
+  $Product2->price = 250000;
+  echo "Game : " . $Product2->getLabel();
--- data/Product.php
+++ data/Product.php
@@ -1,5 +1,12 @@
 <?php
 
   class Product{
+    public  $title = "title",
+            $author = "author",
+            $publisher = "publisher",
+            $price = 0;
 
+    public function getLabel() {
+      return "$this->author, $this->publisher\n";
+    }
   }
